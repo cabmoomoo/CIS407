8294af3d19d45df11d3ba51c3e866840ee923a41
cabmoomoo <68883100+cabmoomoo@users.noreply.github.com>
2024-01-02 14:21:58 -0500

    Week 1 Setup
    Here we go! Week 1 of CIS407, Java Programming 2. Gotta love the good
    ol' "Hello World!"

---
+++ week1LabEnvironment/src/week1LabEnvironment/Week1LabEnvironment.java
@@ -0,0 +1,14 @@
+//Caleb Barbee CIS407 Week 1: Setting Up the Development Environment
+package week1LabEnvironment;
+
+public class Week1LabEnvironment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("This certainly isn't my first Java program,");
+		System.out.println("but it is my first program of CIS407!");
+	}
+
+}
